e1eaf392ef6b3f8af4b05bfeefb912e1c118f294
fatich <fatih@itsmoneo.com>
2024-01-17 08:40:19 +0300

    dockerized project | added event schema and store & list endpoint for events

--- app.js
+++ app.js
@@ -2,18 +2,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql').graphqlHTTP;
 const { buildSchema } = require('graphql');
+const PORT = 3000;
+const mongoose = require('mongoose');
+const Event = require('./models/event');
 
 const app = express();
-
+const DUMMY_EVENTS = [];
 app.use(bodyParser.json());
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        # ... ! means required
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        } 
+    
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
         
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
         schema {
             query: RootQuery
@@ -22,15 +41,45 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return ['Romantic Cooking', 'Sailing', 'All Night Coding']
+            return Event.find().then((events) => {
+                return events.map(event => {
+                    // String casting is not necessary anymore - I guess it's because of the new version of mongoose
+                    return {...event._doc, _id: event._doc._id.toString()};
+                })
+            }).catch((err) => {
+                throw err;
+            })
         },
-        createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+        createEvent: args => {
+            const { eventInput } = args;
+            const event = new Event({
+                title: eventInput.title,
+                description: eventInput.description,
+                price: +eventInput.price,
+                date: new Date(eventInput.date)
+            });
+            return event
+                .save()
+                .then((event) => {
+                    // String casting is not necessary anymore - I guess it's because of the new version of mongoose
+                    return {...event._doc, _id: event.id};
+                })
+                .catch((err) => {
+                console.log(err);
+            })
         }
     },
     graphiql: true
-}));
+    }));
 
-app.listen(3000);
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: false,
+}).then((() => {
+    console.log('Connected to MongoDB');
+})).catch((err) => {
+    console.log('MONGO CONNECTION ERROR:', err);
+})
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
 

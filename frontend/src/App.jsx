50b26555022a83a7c60d6816aeccc12108712e63
fatich <fatih@itsmoneo.com>
2024-01-24 08:44:14 +0300

    added navigation to client side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,26 @@ import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
 import { Auth } from "./pages/auth/Auth"
 import { Event } from "./pages/event/Event"
 import { Booking } from "./pages/booking/Booking"
+import {MainNavigation} from "./components/navigation/MainNavigation.jsx";
+
 
 function App() {
   
 
   return (
     <BrowserRouter>
-      <Routes>
-      <Route
-          path="/"
-          element={<Navigate to="/auth" />}
-        />
-        <Route path="/auth" element={<Auth />}  />
-        <Route path="/events" element={<Event />} />
-        <Route path="/bookings" element={<Booking />} />
-      </Routes>
+      <MainNavigation />
+        <main className={'main-content'}>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Navigate to="/auth" />}
+                />
+                <Route path="/auth" element={<Auth />}  />
+                <Route path="/events" element={<Event />} />
+                <Route path="/bookings" element={<Booking />} />
+            </Routes>
+        </main>
     </BrowserRouter>
   )
 }
---
+++ frontend/src/components/navigation/MainNavigation.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router-dom";
+import './MainNavigation.css'
+
+export const MainNavigation = (props) => {
+    return (
+        <header className={'main-navigation'}>
+            <div className={'main-navigation__logo'}>
+                <h1>Easy Event</h1>
+            </div>
+            <nav className={'main-navigation__items'}>
+                <ul>
+                    <li><NavLink to={'/auth'}>Authenticate</NavLink></li>
+                    <li><NavLink to={'/events'}>Events</NavLink></li>
+                    <li><NavLink to={'/bookings'}>Bookings</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
